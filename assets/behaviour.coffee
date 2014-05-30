0d09cb35e03037c2b9a3af8a3ea50b6056f37ef8
Rares Cosma <office@rarescosma.com>
2014-05-30 23:56:37 +0300

    Add script

---
+++ assets/behaviour.coffee
@@ -0,0 +1,84 @@
+# Note that when compiling with coffeescript, the plugin is wrapped in another
+# anonymous function. We do not need to pass in undefined as well, since
+# coffeescript uses (void 0) instead.
+do ($ = jQuery, window, document) ->
+
+  # window and document are passed through as local variable rather than global
+  # as this (slightly) quickens the resolution process and can be more efficiently
+  # minified (especially when both are regularly referenced in your plugin).
+
+  # Create the defaults once
+  pluginName = "somcSubpages"
+  defaults =
+    debug: false
+    orderSel: '[data-trigger="order"]'
+    expandSel: '[data-trigger="expand"]'
+
+  # The actual plugin constructor
+  class Plugin
+    constructor: (@element, options) ->
+      # jQuery has an extend method which merges the contents of two or
+      # more objects, storing the result in the first object. The first object
+      # is generally empty as we don't want to alter the default options for
+      # future instances of the plugin
+      @settings = $.extend {}, defaults, options
+      @_defaults = defaults
+      @_name = pluginName
+      @init()
+
+    # Simple logger.
+    log: (msg) ->
+      console?.log msg if @settings.debug
+
+    init: ->
+      # Access the DOM element and the options via the instance,
+      # e.g., @element and @settings
+      $(@element).on 'click', @settings.orderSel, (ev) =>
+        @handleOrder(ev)
+
+      $(@element).on 'click', @settings.expandSel, (ev) =>
+        @handleExpandCollapse(ev)
+
+    # Sort subitems by title
+    handleOrder: (ev) ->
+      ev.preventDefault()
+      link = $(ev.target)
+      switch link.attr('data-order')
+        when 'asc' then comp = (a, b) ->
+          (a > b) ? -1 : 1
+        when 'desc' then comp = (a, b) ->
+          (a < b) ? -1 : 1
+        else
+          @log "Invalid sort order"
+          return
+
+      $list = link.closest('nav').next('ul');
+      $sorted = $('>li', link.closest('nav').next('ul')).get().sort (l, r) ->
+        left = $('.media__body', $(l)).text()
+        right = $('.media__body', $(r)).text()
+        comp left, right
+
+      $list.empty().append $sorted
+
+    handleExpandCollapse: (ev) ->
+      ev.preventDefault()
+      link = $(ev.target)
+      action = link.attr('data-action')
+      if action is 'expand'
+        link.parent().next('nav').show().next('ul').show()
+        link.attr('data-action', 'collapse').text('Collapse')
+
+      if action is 'collapse'
+        link.parent().next('nav').hide().next('ul').hide()
+        link.attr('data-action', 'expand').text('Expand')
+
+
+  # A really lightweight plugin wrapper around the constructor,
+  # preventing against multiple instantiations
+  $.fn[pluginName] = (options) ->
+    @each ->
+      unless $.data @, "plugin_#{pluginName}"
+        $.data @, "plugin_#{pluginName}", new Plugin @, options
+
+  $ ->
+    $('.somc-wrap').somcSubpages { debug: true }
---
+++ assets/behaviour.js
@@ -0,0 +1,105 @@
+(function() {
+  (function($, window, document) {
+    var Plugin, defaults, pluginName;
+    pluginName = "somcSubpages";
+    defaults = {
+      debug: false,
+      orderSel: '[data-trigger="order"]',
+      expandSel: '[data-trigger="expand"]'
+    };
+    Plugin = (function() {
+      function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+      }
+
+      Plugin.prototype.log = function(msg) {
+        if (this.settings.debug) {
+          return typeof console !== "undefined" && console !== null ? console.log(msg) : void 0;
+        }
+      };
+
+      Plugin.prototype.init = function() {
+        $(this.element).on('click', this.settings.orderSel, (function(_this) {
+          return function(ev) {
+            return _this.handleOrder(ev);
+          };
+        })(this));
+        return $(this.element).on('click', this.settings.expandSel, (function(_this) {
+          return function(ev) {
+            return _this.handleExpandCollapse(ev);
+          };
+        })(this));
+      };
+
+      Plugin.prototype.handleOrder = function(ev) {
+        var $list, $sorted, comp, link;
+        ev.preventDefault();
+        link = $(ev.target);
+        switch (link.attr('data-order')) {
+          case 'asc':
+            comp = function(a, b) {
+              var _ref;
+              return (_ref = a > b) != null ? _ref : -{
+                1: 1
+              };
+            };
+            break;
+          case 'desc':
+            comp = function(a, b) {
+              var _ref;
+              return (_ref = a < b) != null ? _ref : -{
+                1: 1
+              };
+            };
+            break;
+          default:
+            this.log("Invalid sort order");
+            return;
+        }
+        $list = link.closest('nav').next('ul');
+        $sorted = $('>li', link.closest('nav').next('ul')).get().sort(function(l, r) {
+          var left, right;
+          left = $('.media__body', $(l)).text();
+          right = $('.media__body', $(r)).text();
+          return comp(left, right);
+        });
+        return $list.empty().append($sorted);
+      };
+
+      Plugin.prototype.handleExpandCollapse = function(ev) {
+        var action, link;
+        ev.preventDefault();
+        link = $(ev.target);
+        action = link.attr('data-action');
+        if (action === 'expand') {
+          link.parent().next('nav').show().next('ul').show();
+          link.attr('data-action', 'collapse').text('Collapse');
+        }
+        if (action === 'collapse') {
+          link.parent().next('nav').hide().next('ul').hide();
+          return link.attr('data-action', 'expand').text('Expand');
+        }
+      };
+
+      return Plugin;
+
+    })();
+    $.fn[pluginName] = function(options) {
+      return this.each(function() {
+        if (!$.data(this, "plugin_" + pluginName)) {
+          return $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+        }
+      });
+    };
+    return $(function() {
+      return $('.somc-wrap').somcSubpages({
+        debug: true
+      });
+    });
+  })(jQuery, window, document);
+
+}).call(this);
